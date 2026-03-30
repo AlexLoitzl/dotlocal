d610767a64f6877b3e306cfeef7f9565d01bc232
AlexLoitzl <alexander.loitzl@gmail.com>
2026-03-30 20:08:41 +0200

    Add filen sync

---
+++ bin/filen_sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while read src dst; do
+  rclone sync "$src" "$dst" --backup-dir FilenRemote:/backup --checksum --exclude-from "home/alex/.config/filen/ignorelist"
+done < "home/alex/.config/filen/pairs"
