87e5d0ace77444c44246acf00ac001f3117d2003
AlexLoitzl <alexander.loitzl@gmail.com>
2026-04-06 19:28:50 +0200

    Update paths in filen_sync

--- bin/filen_sync.sh
+++ bin/filen_sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read src dst; do
-  rclone sync "$src" "$dst" --backup-dir FilenRemote:/backup --checksum --exclude-from "home/alex/.config/filen/ignorelist"
-done < "home/alex/.config/filen/pairs"
+  rclone sync "$src" "$dst" --backup-dir FilenRemote:/backup --checksum --exclude-from "/home/alex/.config/filen/ignorelist"
+done < "/home/alex/.config/filen/pairs"
